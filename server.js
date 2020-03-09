3471522123bb7bcd31ccf6eac465260f73d66516
Reva Arora <revaarora.1991@gmail.com>
2020-03-09 19:43:54 +0530

    inital commit

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const expressGraphQL = require("express-graphql");
+const schema = require('./schema.js')
+
+const app = express();
+
+app.use(
+ "/graphql",
+ expressGraphQL({
+     schema,
+   graphiql: true
+ })
+);
+
+app.listen(4000, () => {
+ console.log("Listening");
+});
